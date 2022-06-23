1a54d2570bb9e3dbc1d29a284aabdc7de12b1986
fcogutierrez <fcojavier.gutierrezg@gmail.com>
2022-06-23 18:25:46 +0200

    RED Unit tests for Position constructor with negative coordinates

---
+++ PlumGuide.Rover/PlumGuide.Rover.Core.Tests/PositionTests/ConstructorTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace PlumGuide.Rover.Core.Tests.PositionTests
+{
+    public sealed class ConstructorTests
+    {
+        [Theory]
+        [InlineData(5, -5)]        
+        [InlineData(-10, 10)]
+        [InlineData(-10, -5)]
+
+        public void Given_PositionWithAnyNegativeCoordinate_Should_ThrowAnArgumentOutOfRangeException(int x, int y)
+        {
+            var expectedException = Record.Exception (() => new Position(x, y));
+
+            expectedException.Should().BeAssignableTo<ArgumentOutOfRangeException>();
+        }
+    }
+}
