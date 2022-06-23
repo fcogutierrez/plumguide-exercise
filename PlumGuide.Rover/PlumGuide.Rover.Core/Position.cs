dec527b39b9b4c8360066cf485213ec6a6dfc2e8
fcogutierrez <fcojavier.gutierrezg@gmail.com>
2022-06-23 19:36:09 +0200

    GREEN - Equals method for Position

--- PlumGuide.Rover/PlumGuide.Rover.Core/Position.cs
+++ PlumGuide.Rover/PlumGuide.Rover.Core/Position.cs
@@ -20,5 +20,23 @@
 
         public int X { get; }
         public int Y { get; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null)
+            {
+                return false;
+            }
+
+            if (obj is not Position)
+            {
+                return false;
+            }
+
+            var other = (Position)obj;
+            var result = (X == other.X) && (Y == other.Y);
+
+            return result;
+        }
     }
 }
