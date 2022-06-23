3225517af7b120a7fed7cbe925a78e893d8bcd20
fcogutierrez <fcojavier.gutierrezg@gmail.com>
2022-06-23 19:30:09 +0200

    RED - Tests for Position equality

---
+++ PlumGuide.Rover/PlumGuide.Rover.Core.Tests/PositionTests/EqualsTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace PlumGuide.Rover.Core.Tests.PositionTests
+{
+    public sealed class EqualsTests
+    {
+        [Fact]
+        public void Given_SamePositions_Should_ReturnTrue()
+        {
+            var positionOne = new Position(10, 10);
+            var positionTwo = new Position(10, 10);
+
+            var result = positionOne.Equals(positionTwo);
+
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Given_DifferentPositions_Should_ReturnFalse()
+        {
+            var positionOne = new Position(10, 5);
+            var positionTwo = new Position(5, 10);
+
+            var result = positionOne.Equals(positionTwo);
+
+            result.Should().BeFalse();
+        }
+    }
+}
