556e46c6043439c56384db6bf6f5a80a75370845
fcogutierrez <fcojavier.gutierrezg@gmail.com>
2022-06-23 18:57:04 +0200

    RED - Tests for Grid constructor with valid sizes

--- PlumGuide.Rover/PlumGuide.Rover.Core.Tests/GridTests/ConstructorTests.cs
+++ PlumGuide.Rover/PlumGuide.Rover.Core.Tests/GridTests/ConstructorTests.cs
@@ -11,11 +11,20 @@ namespace PlumGuide.Rover.Core.Tests.GridTests
         [InlineData(0, 10)]
         [InlineData(-10, 10)]
         [InlineData(-10, -5)]
-        public void Given_GridWithNonPositiveWidthOrHeight_Should_ThrowAnArgumentOutOfRangeException(int width, int height)
+        public void Given_GridWithNonPositiveSizes_Should_ThrowAnArgumentOutOfRangeException(int width, int height)
         {
             var expectedException = Record.Exception(() => new Grid(width, height));
 
             expectedException.Should().BeAssignableTo<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void Given_ValidSizes_Should_AssignValues()
+        {
+            var grid = new Grid(100, 50);
+
+            grid.Width.Should().Be(100);
+            grid.Height.Should().Be(50);
         }
     }
 }
