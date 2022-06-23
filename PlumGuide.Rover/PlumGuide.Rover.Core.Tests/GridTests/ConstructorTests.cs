cb7392a12aeceb073194175968f714b5e7ecbe04
fcogutierrez <fcojavier.gutierrezg@gmail.com>
2022-06-23 18:52:53 +0200

    RED - Unit tests for Grid constructor with invalid width or height

---
+++ PlumGuide.Rover/PlumGuide.Rover.Core.Tests/GridTests/ConstructorTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace PlumGuide.Rover.Core.Tests.GridTests
+{
+    public sealed class ConstructorTests
+    {
+        [Theory]
+        [InlineData(5, 0)]
+        [InlineData(5, -5)]
+        [InlineData(0, 10)]
+        [InlineData(-10, 10)]
+        [InlineData(-10, -5)]
+        public void Given_GridWithNonPositiveWidthOrHeight_Should_ThrowAnArgumentOutOfRangeException(int width, int height)
+        {
+            var expectedException = Record.Exception(() => new Grid(width, height));
+
+            expectedException.Should().BeAssignableTo<ArgumentOutOfRangeException>();
+        }
+    }
+}
---
+++ PlumGuide.Rover/PlumGuide.Rover.Core/Grid.cs
@@ -0,0 +1,13 @@
+﻿namespace PlumGuide.Rover.Core
+{
+    public sealed class Grid
+    {
+        public Grid(int width, int height)
+        {
+        }
+
+        public int Width { get; }
+        public int Height { get; }
+    }
+
+}
