defd558feed8464fa9be51c95478d3c8e8c085d0
fcogutierrez <fcojavier.gutierrezg@gmail.com>
2022-06-25 13:09:53 +0200

    REFACTOR - Deleting Move method from Rover

--- PlumGuide.Rover/PlumGuide.Rover.Core/Rover.cs
+++ PlumGuide.Rover/PlumGuide.Rover.Core/Rover.cs
@@ -67,30 +67,5 @@ namespace PlumGuide.Rover.Core
             var newPosition = new Position(newX, newY);
             Position = newPosition;
         }
-
-        public void Move(Direction direction)
-        {
-            var newX = Position.X;
-            var newY = Position.Y;
-
-            switch (Facing)
-            {
-                case Facing.North:
-                    newY = direction == Direction.Forward ? Position.Y + 1 : Position.Y - 1;
-                    break;
-                case Facing.South:
-                    newY = direction == Direction.Forward ? Position.Y - 1 : Position.Y + 1;
-                    break;
-                case Facing.East:
-                    newX = direction == Direction.Forward ? Position.X + 1 : Position.X - 1;
-                    break;
-                case Facing.West:
-                    newX = direction == Direction.Forward ? Position.X - 1 : Position.X + 1;
-                    break;
-            }
-
-            var newPosition = new Position(newX, newY);
-            Position = newPosition;
-        }
     }
 }
