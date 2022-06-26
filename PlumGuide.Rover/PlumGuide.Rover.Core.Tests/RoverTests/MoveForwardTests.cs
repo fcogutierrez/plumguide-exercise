b0291faf2255a14a702fded2ff7e3cdbfe4de8d8
fcogutierrez <fcojavier.gutierrezg@gmail.com>
2022-06-26 15:56:23 +0200

    REFACTOR - Refactoring tests for MoveForward and MoveBackward

--- PlumGuide.Rover/PlumGuide.Rover.Core.Tests/RoverTests/MoveForwardTests.cs
+++ PlumGuide.Rover/PlumGuide.Rover.Core.Tests/RoverTests/MoveForwardTests.cs
@@ -11,7 +11,7 @@ namespace PlumGuide.Rover.Core.Tests.RoverTests
         [InlineData(Facing.South, 5, 4)]
         [InlineData(Facing.East, 6, 5)]
         [InlineData(Facing.West, 4, 5)]
-        public void Given_PositionAndFacing_Should_ChangeToExpectedPositionAndDoNotChangeFacing(Facing facing, int expectedX, int expectedY)
+        public void Given_PositionAndFacing_Should_MoveToTheExpectedPosition(Facing facing, int expectedX, int expectedY)
         {
             var initialPosition = new Position(5, 5);
             var expectedPosition = new Position(expectedX, expectedY);
