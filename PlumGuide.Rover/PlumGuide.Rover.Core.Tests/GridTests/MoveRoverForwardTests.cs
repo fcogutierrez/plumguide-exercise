58e70de13588f1bd1a7e30d24d59c4e9895abe43
fcogutierrez <fcojavier.gutierrezg@gmail.com>
2022-06-26 16:19:16 +0200

    RED - Tests for MoveRoverForward when the next move is allowed

--- PlumGuide.Rover/PlumGuide.Rover.Core.Tests/GridTests/MoveRoverForwardTests.cs
+++ PlumGuide.Rover/PlumGuide.Rover.Core.Tests/GridTests/MoveRoverForwardTests.cs
@@ -11,8 +11,8 @@ namespace PlumGuide.Rover.Core.Tests.GridTests
         [InlineData(Facing.North, 5, 49)]
         [InlineData(Facing.South, 5, 0)]
         [InlineData(Facing.East, 49, 5)]
-        [InlineData(Facing.West, 0, 5)]        
-        public void Given_RoverInAValidPosition_Should_ThrowAMoveNotAllowedExceptionIfTheRequestedMoveIsNotAllowed(Facing facing, int currentX, int currentY)
+        [InlineData(Facing.West, 0, 5)]
+        public void Given_RoverInAValidPosition_Should_ThrowAMoveNotAllowedExceptionIfTheNewPositionIsNotAllowed(Facing facing, int currentX, int currentY)
         {
             var rover = new Rover(new Position(currentX, currentY), facing);
             var grid = new Grid(50, 50, rover);
@@ -20,6 +20,17 @@ namespace PlumGuide.Rover.Core.Tests.GridTests
             var expectedException = Record.Exception(() => grid.MoveRoverForward());
 
             expectedException.Should().BeAssignableTo<MoveNotAllowedException>();
+        }
+
+        [Fact]
+        public void Given_RoverInAValidPosition_Should_ProcessTheMoveIfTheNewPositionIsAllowed()
+        {
+            var rover = new Rover(new Position(25, 25), Facing.North);
+            var grid = new Grid(50, 50, rover);
+
+            grid.MoveRoverForward();
+
+            grid.Rover.Position.Should().NotBe(new Position(25, 25));
         }
     }
 }
