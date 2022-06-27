ccb9cb4907f817424c82f4a2851fdfea1af5cc4d
fgutierrez-socialicity <franciscogutierrez@socialicity.co.uk>
2022-06-27 13:10:42 +0200

    RED - Tests to handle a correct RoadMap within the MoveRoverCommandHandler

---
+++ PlumGuide.Rover/PlumGuide.Rover.Application.Tests/MoveRoverCommandHandlerTests/HandleTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using PlumGuide.Rover.Application.Commands;
+using PlumGuide.Rover.Core;
+using PlumGuide.Rover.Core.Enums;
+using Xunit;
+
+namespace PlumGuide.Rover.Application.Tests.MoveRoverCommandHandlerTests
+{
+    public sealed class HandleTests
+    {
+        [Theory]
+        [InlineData(0, 0, Facing.North, "FFRFF", 2, 2, Facing.East)]
+        [InlineData(0, 0, Facing.North, "FRFLFF", 1, 3, Facing.North)]
+        public void Given_RoverWithinAGrid_Should_BeMovedToTheExpectedPosition(int initialX, int initialY, Facing initialFacing, string roadMap, int expectedX, int expectedY, Facing expectedFacing)
+        {
+            var initialPosition = new Position(initialX, initialY);
+            var rover = new Core.Rover(initialPosition, initialFacing);
+            var grid = new Grid(50, 50, rover);
+
+            var sut = new MoveRoverCommandHandler(grid);
+            var command = new MoveRoverCommand(roadMap);
+
+            sut.Handle(command);
+
+            var expectedPosition = new Position(expectedX, expectedY);
+
+            sut.Grid.Rover.Position.Should().Be(expectedPosition);
+            sut.Grid.Rover.Facing.Should().Be(expectedFacing);
+        }
+    }
+}
