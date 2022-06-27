bd0e71e2ba5023c69b8ce962f15606b30d86f418
fgutierrez-socialicity <franciscogutierrez@socialicity.co.uk>
2022-06-27 12:45:57 +0200

    RED - Tests for constructor of the CommandHandler

---
+++ PlumGuide.Rover/PlumGuide.Rover.Application.Tests/MoveRoverCommandHandlerTests/ConstructorTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace PlumGuide.Rover.Application.Tests.MoveRoverCommandHandlerTests
+{
+    public sealed class ConstructorTests
+    {
+        [Fact]
+        public void Given_MissingGrid_Should_ThrowAnArgumentNullException()
+        {
+            var expectedException = Record.Exception(() => new MoveRoverCommandHandler(default));
+
+            expectedException.Should().BeAssignableTo<ArgumentNullException>();
+        }
+    }
+}
--- PlumGuide.Rover/PlumGuide.Rover.Application/MoveRoverCommandHandler.cs
+++ PlumGuide.Rover/PlumGuide.Rover.Application/MoveRoverCommandHandler.cs
@@ -1,9 +1,17 @@
 ﻿using PlumGuide.Rover.Application.Commands;
+using PlumGuide.Rover.Core;
 
 namespace PlumGuide.Rover.Application
 {
     public sealed class MoveRoverCommandHandler
     {
+        public MoveRoverCommandHandler(Grid grid)
+        {
+            
+        }
+
+        public Grid Grid { get; }
+
         public void Handle(MoveRoverCommand command)
         {
 
